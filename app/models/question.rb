bfff96887d9edd7edc1d7130dd74576a2d1c8305
Timothy Jaeger <tim.jaeger>
2012-02-03 12:32:11 -0500

    (New) initial commit

---
+++ app/models/question.rb
@@ -0,0 +1,4 @@
+class Question < ActiveRecord::Base
+  belongs_to :admin
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'b31fee1c4af15169da87527796cd36cda66710903372988062ffb52b9d3edd92f62923d61a30d8853efd427c9eb9d34a26fbc5c4722f6cee3fba22687da10907'
