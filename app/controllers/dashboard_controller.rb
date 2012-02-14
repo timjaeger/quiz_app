59237cf0d8076184e4686d25ca86347e7d7f98d8
Timothy Jaeger <tim.jaeger>
2012-02-14 15:28:56 -0500

    Dashboard leaderboard now working properly

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,18 @@
 class DashboardController < ApplicationController
 
   def index
-    @users = Point.find(:all, 
-                        :group => 'user_id',
-                        :order => 'user_id DESC', :conditions => { :correct => "yes"})
+    
+    #@users = Point.find(:all, 
+     #                   :group => 'user_id',
+      #                  :order => 'correct DESC', :conditions => { :correct => "yes"})
+       
+       #@users = Point.count(:group => :user_id, :conditions => { :correct => "yes"})
+       @users = Point.all(:select => "*, COUNT(user_id) as count", :conditions => { :correct => 'yes' }, :group => :user_id, :order => 'count desc')
+       
+       #@users = Point.all(:select => 'user_id, count(id) as point_count', :group => :user_id, :conditions => { :correct => 'yes' }, :order => 'point_count desc', :limit => 10)
+       
+                                         
+       @question = Question.find(2)
   end
 
 end
