668747ca541a827c7478404e4a96a01342b89b7c
Timothy Jaeger <tim.jaeger>
2012-02-15 11:55:52 -0500

    Added rounded corners and top 5 salespeople to dashboard

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,6 +1,7 @@
 class ChartsController < ApplicationController
   
   def index
+     @users = Point.all(:select => "*, COUNT(user_id) as count", :conditions => { :correct => 'yes' }, :group => :user_id, :order => 'count desc')
   end
   
 end
